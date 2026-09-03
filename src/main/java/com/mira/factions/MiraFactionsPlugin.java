b29edb5b295849b4caba9a9894c4cdf13e119db7
FiveS <sgmmsionide@gmail.com>
2026-09-04 05:30:25 +1000

    Register admin claiming and protected zone guards

--- src/main/java/com/mira/factions/MiraFactionsPlugin.java
+++ src/main/java/com/mira/factions/MiraFactionsPlugin.java
@@ -43,6 +43,8 @@ public final class MiraFactionsPlugin extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new FactionHistoryTabListener(), this);
         getServer().getPluginManager().registerEvents(new FactionAdminAuditListener(this, factions, history), this);
         getServer().getPluginManager().registerEvents(new FactionListener(this, factions, gui), this);
+        getServer().getPluginManager().registerEvents(new ProtectedZoneListener(this, factions), this);
+        getServer().getPluginManager().registerEvents(new AdminFactionClaimListener(this, factions), this);
         getServer().getPluginManager().registerEvents(new FactionFlightDelegationListener(this, factions), this);
         getServer().getPluginManager().registerEvents(new FactionUpgradeAuditListener(this, factions, history), this);
         getServer().getPluginManager().registerEvents(new FactionUpgradeListener(this, factions), this);
