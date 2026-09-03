774270fe724907e16aa7c4fed8551022d6d11a9b
FiveS <sgmmsionide@gmail.com>
2026-09-03 11:51:21 +1000

    Fix inventory holder import

--- src/main/java/com/mira/factions/listener/FactionListener.java
+++ src/main/java/com/mira/factions/listener/FactionListener.java
@@ -14,6 +14,7 @@ import org.bukkit.event.entity.*;
 import org.bukkit.event.inventory.*;
 import org.bukkit.event.player.*;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.*;
