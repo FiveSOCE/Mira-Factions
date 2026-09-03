4c306f829b1f35670964a256e72ac8ac9d7badad
FiveS <sgmmsionide@gmail.com>
2026-09-03 11:43:38 +1000

    Prepare MiraFactions v0.2.0 dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,19 @@
 plugins { java }
 
 group = "com.mira"
-version = "0.1.0"
+version = "0.2.0"
 
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://jitpack.io")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+    compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 java { toolchain.languageVersion.set(JavaLanguageVersion.of(21)) }
